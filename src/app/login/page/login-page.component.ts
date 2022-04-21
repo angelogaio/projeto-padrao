9459523225c8f71c638675c31e39c6c5f0f21026
angelogaio <angelo.gaio@hotmail.com>
2022-04-21 17:21:15 -0300

    criado tela de login

--- src/app/login/page/login-page.component.ts
+++ src/app/login/page/login-page.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  ChangeDetectionStrategy,
+  Injectable,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
+@Injectable({
+  providedIn: 'root',
+})
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginPageComponent implements OnInit {
+  loading$ = false;
+  @Input() error: string | null = '';
+  @Output() submitEM = new EventEmitter();
 
-  constructor() { }
+  backgroundPhoto: any = 'assets/image/backgroud/construction_640x853.jpg';
+  // logoPhoto:any = 'assets/imagens/logo.png';
 
-  ngOnInit(): void {
-  }
+  formGroup = this.fb.group({
+    username: [null, [Validators.required]],
+    password: [null, [Validators.required]],
+  });
 
+  constructor(private fb: FormBuilder, private router: Router,) {}
+
+  ngOnInit(): void {}
 }
