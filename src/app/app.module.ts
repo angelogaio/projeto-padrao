d0e3a0787b2b49df50afe720567af60fb745d43e
angelogaio <angelo.gaio@hotmail.com>
2022-05-07 19:32:45 -0300

    melhoria layout login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
-import { LoginModule } from './login/login.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 
@@ -18,6 +18,7 @@ import { LoginModule } from './login/login.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
